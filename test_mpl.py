94ec5b8c29df3eefa2a43c075b2c510c449a5ce5
bermmerb <bermmerb@gmail.com>
2022-06-13 11:56:56 +0700

    initial_version

---
+++ test_mpl.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+data1 = np.random.normal(0, 0.1, 1000)
+data2 = np.random.normal(1, 0.4, 1000) + np.linspace(0, 1, 1000)
+data3 = 2 + np.random.random(1000) * np.linspace(1, 5, 1000)
+data4 = np.random.normal(3, 0.2, 1000) + 0.3 * np.sin(np.linspace(0, 20, 1000))
+
+data = np.vstack([data1, data2, data3, data4]).transpose()
+
+df = pd.DataFrame(data, columns=['data1', 'data2', 'data3', 'data4'])
+
+plt.plot(df)
+plt.title('Line plot')
+plt.legend(['data1', 'data2', 'data3', 'data4'])
+
+plt.show()
